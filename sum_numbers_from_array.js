978ad2ca712dc9db138f78ff3352c1e749438c70
ShwetaRK <kumari.shweta.in@gmail.com>
2019-05-08 00:24:40 +0530

    Added method to solve the scenario

--- sum_numbers_from_array.js
+++ sum_numbers_from_array.js
@@ -4,3 +4,17 @@
 * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 */
 
+
+/*This is the first approach and simplest method to resolve issue: Brute force method*/
+function sumOfTwoNumbersInArray (inputArray, sumValue) {
+  var isSumOfTwoNumbers = false;
+  for(var i=0; i < inputArray.length; i++){
+    if(inputArray[i] < sumValue){
+      let searchVal = sumValue - inputArray[i];
+      isSumOfTwoNumbers = inputArray.find(function(element) {
+        return element === searchVal;
+      });
+    }
+  }
+  return isSumOfTwoNumbers;
+}
